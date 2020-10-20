e6546fbe88faee59beff10a4d5d3acd8e9aeead7
goodove <73025275+goodove@users.noreply.github.com>
2020-10-20 11:36:18 +0800

    Update jump7.py

--- jump7.py
+++ jump7.py
@@ -1,2 +1,20 @@
-print('Hello world')
-
+import random
+WORDS=("python","jumble","easy","difficult","answer","continue","phone","position","game")
+print("欢迎参加猜单词游戏,把字母组合成一个正确的单词")
+iscontinue="y"
+while iscontinue=="y" or iscontinue=="Y":
+  word=random.choice(WORDS)
+  correct=word
+  jumble=""
+  while word:
+    position=random.randrange(len(word))
+    jumble+=word[position]
+    word=word[:position]+word[(position+1):]
+  print("乱序后单词：",jumble)
+  guess=input("\n请你猜:")
+  while guess !=correct and guess !="":
+    print("对不起不正确")
+    guess=input("继续猜：")
+  if guess==correct:
+    print("真棒，你猜对了!\n")
+  iscontinue = input("\n\n是否继续（Y/N）:")
